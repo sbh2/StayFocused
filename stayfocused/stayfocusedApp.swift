e96028a1e4103ad7fdc57f61dbec04c433b1bbb8
Sarah Alharbi <sbalharbi22@twq.idserve.net>
2023-01-14 21:16:50 +0300

    Initial Commit

---
+++ stayfocused/stayfocusedApp.swift
@@ -0,0 +1,17 @@
+//
+//  stayfocusedApp.swift
+//  stayfocused
+//
+//  Created by Sara bayan alharbi on 21/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct stayfocusedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
